be785322bc1bfb4ca9395e82d10f577e2449c1e3
bit <bit@DESKTOP-JL3FM1E>
2019-02-21 13:08:18 +0900

    완료

--- src/main/java/problem01/Main.java
+++ src/main/java/problem01/Main.java
@@ -8,6 +8,20 @@ public class Main {
 
 	public static void findMaxProduct(int[] arr) {
 		
+		int gop = 0;
+		int Max = 0;
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr.length; j++) 
+			{
+				gop = arr[i] * arr[j];
+						if(gop > Max) {
+							Max = gop;
+						}
+			}
+		}
+		
 		//
 		// 코드를 완성 하십시오.
 		//
